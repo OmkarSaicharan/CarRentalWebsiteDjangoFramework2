e5491ee99770afb3943a5fa606e342503639bfcc
omkar <161125388+OmkarSaicharan@users.noreply.github.com>
2024-08-06 20:30:57 +0530

    file

--- car/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class registration(models.Model):
-    first_name=models.CharField(max_length=20)
-    last_name=models.CharField(max_length=20)
-    email=models.CharField(max_length=40)
-    password=models.CharField(max_length=50)
-
-# Create your models here.
